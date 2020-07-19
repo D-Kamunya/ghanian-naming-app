1fdd0c28113764ed9ce082614e04f538ca17b147
Dennis Kamunya <dennismuriithik@gmail.com>
2020-07-19 09:22:40 +0300

    Set dom elements as variables

---
+++ js/index.js
@@ -0,0 +1,6 @@
+//SET DOM ELEMENTS AS VARIABLES
+const butTn=document.getElementById('submitt')
+const birthDay=document.getElementById('birthday')
+const birthMnt=document.getElementById('birthmnt')
+const birthYr=document.getElementById('birthyr')
+const genders=document.getElementsByName('gender')
