c5550ca9af78f18f98281067ae3ba7a0a256c838
Dennis Kamunya <dennismuriithik@gmail.com>
2020-07-19 09:24:13 +0300

    Set array variables for weekdays names and akan names

--- js/index.js
+++ js/index.js
@@ -4,3 +4,14 @@ const birthDay=document.getElementById('birthday')
 const birthMnt=document.getElementById('birthmnt')
 const birthYr=document.getElementById('birthyr')
 const genders=document.getElementsByName('gender')
+
+//Set array variables for weekdays names and akan names
+const daysofWk=[
+  "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+]
+const maleAkanNames=[
+  "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
+]
+const femaleAkanNames=[
+  "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+]
